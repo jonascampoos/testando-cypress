310abe115e4fff747804573a3b92014a940bd1e6
jonas <jonascamposk7ng.gmail.com>
2025-07-31 23:18:38 -0300

    adding baseUrl

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -11,17 +11,16 @@ const selectorlist = {
 }
 
   it('login - sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorlist.usernamefield).type(userData.usersucess.username)
     cy.get(selectorlist.passwordfield).type(userData.usersucess.password)
     cy.get(selectorlist.loginbutton).click()
     cy.get(selectorlist.SelectiontitleTopbar)
   })
    it('login - fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorlist.usernamefield).type(userData.userfail.username)
-    cy.get(selectorlist.passwordfield).type(userData.userfail.password
-    )
+    cy.get(selectorlist.passwordfield).type(userData.userfail.password)
     cy.get(selectorlist.loginbutton).click()
     cy.get(selectorlist.wrongcredentialAlert)
   })
