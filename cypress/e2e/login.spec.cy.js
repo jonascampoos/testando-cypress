705065ed9c849e64a87376501265655c987bf3de
jonas <jonascamposk7ng.gmail.com>
2025-08-05 22:05:03 -0300

    alterando dados de cadastro

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -14,7 +14,8 @@ const selectorlist = {
   lastnamefield: "[name='lastName']",
   genericfield: ".oxd-input",
   datafield: "[placeholder='yyyy-dd-mm']",
-  closebutton: ".--close"
+  closebutton: ".--close",
+  savebutton: "[type='submit']"
 }
 
   it.only('login - sucess', () => {
@@ -24,14 +25,15 @@ const selectorlist = {
     cy.get(selectorlist.loginbutton).click()
     cy.get(selectorlist.SelectiontitleTopbar)
     cy.get(selectorlist.myinfobutton). click()
-    cy.get(selectorlist.firstnamefield).type("jonas")
-    cy.get(selectorlist.middlenamefield).type("campos")
-    cy.get(selectorlist.lastnamefield).type("costa")
-    cy.get(selectorlist.genericfield).eq(3).type("lonhin")
-    cy.get(selectorlist.genericfield).eq(4).type("1254")
-    cy.get(selectorlist.genericfield).eq(5).type("45554")
-    cy.get(selectorlist.datafield).eq(0).type("2025-03-01")
+    cy.get(selectorlist.firstnamefield).clear ().type("jonas")
+    cy.get(selectorlist.middlenamefield).clear ().type("campos")
+    cy.get(selectorlist.lastnamefield).clear ().type("costa")
+    cy.get(selectorlist.genericfield).eq(4).clear ().type("lonhin")
+    cy.get(selectorlist.genericfield).eq(5).clear ().type("1254")
+    cy.get(selectorlist.genericfield).eq(6).clear ().type("45554")
+    cy.get(selectorlist.datafield).eq(0).clear ().type("2025-03-01")
     cy.get(selectorlist.closebutton). click()
+    cy.get(selectorlist.savebutton).eq(0). click()
   })
    it('login - fail', () => {
     cy.visit('/auth/login')
